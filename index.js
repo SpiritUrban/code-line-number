4962c54b5dc2eccf28d27294d832cc29eebea1f9
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2022-05-09 17:38:37 +0300

    all

---
+++ index.js
@@ -0,0 +1,36 @@
+const l = console.log;
+
+module.exports = () => {
+
+    var err = new Error(); // err = a lot of text strings (stack)
+
+    //
+    // the IE crutch - IE requires the Error to actually be throw  else is undefined.
+    //
+    const ieCrutch = () => {
+        try {
+            throw err;
+        } catch (err) {
+            if (!err.stack) return 0; // probably IE <10
+        }
+    }
+    if (!err.stack) ieCrutch();
+
+    var stack = err.stack.toString().split(/\r\n|\n/); // convert text to array of text lines
+
+    var frameRE = /:(\d+):(?:\d+)[^\d]*$/;
+    l(frameRE)
+
+    const result = stack.find(line => {
+        l('*', !frameRE.exec(line))
+        return frameRE.exec(line)
+    })
+    l('result ', result)
+
+    const parsed = frameRE.exec(result);
+    const desired = parsed[1]
+
+    l('x-',)
+
+    return desired;
+}
