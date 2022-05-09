7b3d666f1da57ab3580dc6b466e9190f7b848bbe
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2022-05-09 20:18:24 +0300

    refactor

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
 const l = console.log;
 
+/**
+* the IE crutch - IE requires the Error to actually be throw  else is undefined.
+*/
+const ieCrutch = (err) => { try { throw err } catch (err) { return err } };
+
+
 module.exports = () => {
-
     var err = new Error(); // err = a lot of text strings (stack)
-
-    //
-    // the IE crutch - IE requires the Error to actually be throw  else is undefined.
-    //
-    const ieCrutch = () => { try { throw err } catch (err) { } };
-
-    if (!err.stack) ieCrutch();
+    if (!err.stack) err = ieCrutch(err);
     if (!err.stack) return 0; // probably IE <10
-
-    var stack = err.stack.toString().split(/\r\n|\n/); // convert text to array of text lines
-
-    var frameRE = /:(\d+):(?:\d+)[^\d]*$/;
-    l(frameRE)
-
-    const result = stack.find(line => {
-        l('*', !frameRE.exec(line))
-        return frameRE.exec(line)
-    })
-    l('result ', result)
-
-    const parsed = frameRE.exec(result);
+    const stack = err.stack.toString().split(/\r\n|\n/); // convert text to array of text lines
+    const re = /:(\d+):(?:\d+)[^\d]*$/;
+    const firstLne = stack.find(line => re.exec(line));
+    const parsed = re.exec(firstLne);
     const desired = parsed[1]
-
-    l('x-', { parsed, desired })
-
+    // l('x-', { firstLne, parsed, desired })
     return desired;
 }
