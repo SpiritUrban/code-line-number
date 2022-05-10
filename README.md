b1e67b818675a461aa2e28d0ccefae6bb277b6e1
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2022-05-10 11:07:01 +0300

    improvement

--- README.md
+++ README.md
@@ -14,7 +14,12 @@ $ npm install code-line-number
 
 ```js
 const ln = require('code-line-number');
-ln() // => 20
-ln('report') // => 20, more data in console/terminal
+// or
+import ln from 'code-line-number';
+
+ln(2); // => 20
+// ln(2: Is a depth level, can be 3 or other );
+ln(2, 'report'); // => 20, more data in console/terminal
+// You can use 'report' for watching call stack and choose depth level you need
 ```
 
--- index.js
+++ index.js
@@ -5,17 +5,15 @@ const l = console.log;
 */
 const ieCrutch = (err) => { try { throw err } catch (err) { return err } };
 
-module.exports = (...commands) => {
+module.exports = (depthLevel, command) => {
     var err = new Error(); // err = a lot of text strings (stack)
     if (!err.stack) err = ieCrutch(err);
     if (!err.stack) return 0; // probably IE <10
     const stack = err.stack.toString().split(/\r\n|\n/); // convert text to array of text lines
-    const re = /:(\d+):(?:\d+)[^\d]*$/;
-    const firstLne = stack.find(line => re.exec(line));
-    const parsed = re.exec(firstLne);
-    const desired = parsed[1];
-    commands.forEach(command=>{
-        if (command=='report') l('bunch:', { firstLne, parsed, desired });
-    });
-    return desired;
+    const nextLne = stack[depthLevel];
+    const whoDirtyArr = nextLne.split(':');
+    whoDirtyArr.pop();
+    const line = whoDirtyArr.pop();
+    if (command == 'report') l('bunch:', { nextLne, stack });
+    return line
 };
